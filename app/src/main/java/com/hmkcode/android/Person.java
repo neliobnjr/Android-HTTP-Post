9f22f5fb23c34c3769f9598100af4214b48a742e
Anantha Raju C <arcswdev@gmail.com>
2016-03-03 14:41:08 +0530

    initial commit - everything works

---
+++ app/src/main/java/com/hmkcode/android/Person.java
@@ -0,0 +1,39 @@
+package com.hmkcode.android;
+
+public class Person
+{
+    private String name;
+    private String country;
+    private String twitter;
+
+    public String getTwitter()
+    {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter)
+    {
+        this.twitter = twitter;
+    }
+
+    public String getCountry()
+    {
+        return country;
+    }
+
+    public void setCountry(String country)
+    {
+        this.country = country;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+}
